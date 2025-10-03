c195c86cc53d5b09c6d8cf9e00460095e6ba6e97
Giovanni Rella <ap82@cdc.gov>
2025-10-03 14:46:07 -0400

    trying /app workdir

--- src/cfa_dagster/azure_batch/executor.py
+++ src/cfa_dagster/azure_batch/executor.py
@@ -262,7 +262,7 @@ class AzureBatchStepHandler(StepHandler):
         )
         container_settings = TaskContainerSettings(
             image_name=step_image,
-            container_run_options="--rm",
+            container_run_options="--rm --workdir /app",
             registry=container_registry,
         )
 
