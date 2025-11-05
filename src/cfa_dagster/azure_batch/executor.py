8d56b112e0441ed00bc1da8b705f69429363c807
Giovanni Rella <ap82@cdc.gov>
2025-11-05 14:07:30 -0500

    removing step key from Batch job_id

--- src/cfa_dagster/azure_batch/executor.py
+++ src/cfa_dagster/azure_batch/executor.py
@@ -247,7 +247,7 @@ class AzureBatchStepHandler(StepHandler):
         step_key = self._get_step_key(step_handler_context)
         execute_step_args = step_handler_context.execute_step_args
 
-        job_id = f"dagster-step-{step_handler_context.dagster_run.run_id[:8]}-{step_key}"
+        job_id = f"dagster-run-{step_handler_context.dagster_run.run_id[:8]}"
         self._step_job_ids[step_key] = job_id
 
         pool_info = PoolInformation(pool_id=self._pool_id)
