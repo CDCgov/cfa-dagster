d507a7359cab4f22d74cf3b4477dda0305802f3e
Giovanni Rella <ap82@cdc.gov>
2026-03-19 17:25:23 -0400

    updating examples

--- examples/dagster_defs.py
+++ examples/dagster_defs.py
@@ -6,10 +6,9 @@
 #    "dagster-azure>=0.27.4",
 #    "dagster-docker>=0.27.4",
 #    "dagster-postgres>=0.27.4",
-#    "dagster-webserver==1.12.2",
-#    "dagster==1.12.2",
+#    "dagster-webserver>=1.12.2",
+#    "dagster>=1.12.2",
 #    "cfa-dagster @ git+https://github.com/cdcgov/cfa-dagster.git",
-#    "pyyaml>=6.0.2",
 # ]
 # ///
 
@@ -27,17 +26,17 @@ from dagster_azure.blob import (
 from cfa_dagster import (
     ADLS2PickleIOManager,
     AzureContainerAppJobRunLauncher,
+    DynamicGraphAssetExecutionContext,
     ExecutionConfig,
     SelectorConfig,
     azure_batch_executor,
+    azure_container_app_job_executor,
     collect_definitions,
     docker_executor,
     dynamic_executor,
+    dynamic_graph_asset,
     is_production,
     start_dev_env,
-)
-from cfa_dagster import (
-    azure_container_app_job_executor as azure_caj_executor,
 )
 
 # function to start the dev server
@@ -49,62 +48,12 @@ IMAGE_REGISTRY = "cfaprdbatchcr"
 image_tag = "latest" if is_production() else user
 image = f"{IMAGE_REGISTRY}.azurecr.io/cfa-dagster:{image_tag}"
 
-
-@dg.asset(
-    kinds={"azure_blob"},
-    description="An asset that downloads a file from Azure Blob Storage",
-)
-def basic_blob_asset(azure_blob_storage: AzureBlobStorageResource):
-    """
-    An asset that downloads a config file from Azure Blob
-    """
-    container_name = "cfadagsterdev"
-    with azure_blob_storage.get_client() as blob_storage_client:
-        container_client = blob_storage_client.get_container_client(
-            container_name
-        )
-    downloader = container_client.download_blob("test-files/test_config.json")
-    print("Downloaded file from blob!")
-    return downloader.readall().decode("utf-8")
-
-
-@dg.asset(
-    description="An asset that runs R code",
-)
-def basic_r_asset(basic_blob_asset):
-    subprocess.run("Rscript hello.R", shell=True, check=True)
-
-    # Read the random number from output.txt
-    with open("output.txt", "r") as f:
-        random_number = f.read().strip()
-
-    return dg.MaterializeResult(
-        metadata={
-            # add metadata from upstream asset
-            "config": dg.MetadataValue.json(json.loads(basic_blob_asset)),
-            # Dagster will plot numeric values as you repeat runs
-            "output_value": dg.MetadataValue.int(int(random_number)),
-        }
-    )
-
-
-# partitions are parameters to your asset function that trigger parallel compute
-disease_partitions = dg.StaticPartitionsDefinition(["COVID", "FLU", "RSV"])
-
-
-@dg.asset(
-    description="A partitioned asset that runs R code for different diseases",
-    partitions_def=disease_partitions,
-)
-def partitioned_r_asset(context: dg.OpExecutionContext):
-    disease = context.partition_key
-    subprocess.run(f"Rscript hello.R {disease}", shell=True, check=True)
-
+# ----------------
+# Execution Config - defines where your workflows run
+# ----------------
 
 # this should match your Dockerfile WORKDIR
 workdir = "/app"
-
-# image = f"cfaprdbatchcr.azurecr.io/cfa-dagster:{user}"
 
 # this is the default run config that launches the job in your local shell
 # and executes each step in a separate system process
@@ -142,7 +91,7 @@ docker_config = ExecutionConfig(
 # add this to a job or the Definitions class to use it
 azure_caj_config = ExecutionConfig(
     executor=SelectorConfig(
-        class_name=azure_caj_executor.__name__,
+        class_name=azure_container_app_job_executor.__name__,
         config={
             "container_app_job_name": "cfa-dagster",
             # specify a default image
@@ -183,34 +132,69 @@ azure_batch_config = ExecutionConfig(
     ),
 )
 
-# jobs are used to materialize assets with a given configuration
-basic_r_asset_job = dg.define_asset_job(
-    name="basic_r_asset_job",
-    selection=dg.AssetSelection.assets(basic_r_asset),
-    # tag the run with your user to allow for easy filtering in the Dagster UI
-    tags={"user": user},
-    config=dg.RunConfig(
-        # try switching to Azure compute after pushing your image
-        execution=docker_config.to_run_config()
-        # execution=azure_batch_config.to_run_config()
-        # execution=azure_caj_config.to_run_config()
-    ),
-    executor_def=dynamic_executor(),
-)
 
-partitioned_r_asset_job = dg.define_asset_job(
-    name="partitioned_r_asset_job",
-    selection=dg.AssetSelection.assets(partitioned_r_asset),
-    # tag the run with your user to allow for easy filtering in the Dagster UI
-    tags={"user": user},
-    config=dg.RunConfig(
-        # try switching to Azure compute after pushing your image
-        execution=docker_config.to_run_config()
-        # execution=azure_batch_config.to_run_config()
-        # execution=azure_caj_config.to_run_config()
-    ),
-    executor_def=dynamic_executor(),
+# ----------------
+# Assets - operations that produce tracked artifacts
+# ----------------
+
+
+@dg.asset(
+    kinds={"azure_blob"},
+    description="An asset that downloads a file from Azure Blob Storage",
 )
+def basic_blob_asset(azure_blob_storage: AzureBlobStorageResource):
+    """
+    An asset that downloads a config file from Azure Blob
+    """
+    container_name = "cfadagsterdev"
+    with azure_blob_storage.get_client() as blob_storage_client:
+        container_client = blob_storage_client.get_container_client(
+            container_name
+        )
+    downloader = container_client.download_blob("test-files/test_config.json")
+    print("Downloaded file from blob!")
+    return downloader.readall().decode("utf-8")
+
+
+@dg.asset(
+    description="An asset that runs R code",
+)
+def basic_r_asset(basic_blob_asset):
+    subprocess.run("Rscript hello.R", shell=True, check=True)
+
+    # Read the random number from output.txt
+    with open("output.txt", "r") as f:
+        random_number = f.read().strip()
+
+    return dg.MaterializeResult(
+        metadata={
+            # add metadata from upstream asset
+            "config": dg.MetadataValue.json(json.loads(basic_blob_asset)),
+            # Dagster will plot numeric values as you repeat runs
+            "output_value": dg.MetadataValue.int(int(random_number)),
+        }
+    )
+
+
+class ParallelRAssetConfig(dg.Config):
+    disease: list[str] = ["COVID", "FLU", "RSV"]
+
+
+@dynamic_graph_asset(
+    graph_dimensions=["disease"],
+    description="A parallel asset that runs R code for different diseases",
+)
+def parallel_r_asset(
+    context: DynamicGraphAssetExecutionContext,
+    config: ParallelRAssetConfig,
+):
+    disease = context.graph_dimension["disease"]
+    subprocess.run(f"Rscript hello.R {disease}", shell=True, check=True)
+
+
+# ------------------------------------------------
+# Jobs - tasks that don't produce tracked artifacts
+# ------------------------------------------------
 
 
 @dg.op
@@ -241,19 +225,23 @@ def build_image_job():
     build_image()
 
 
-# schedule a job to run weekly
+# --------------------------------------------
+# Schedules - run your workflows automatically
+# --------------------------------------------
 schedule_every_wednesday = dg.ScheduleDefinition(
-    name="weekly_cron", cron_schedule="0 9 * * 3", job=basic_r_asset_job
+    name="weekly_cron",
+    cron_schedule="0 9 * * 3",
+    target=[basic_blob_asset, basic_r_asset],
 )
 
 
 # change storage accounts between dev and prod
 storage_account = "cfadagster" if is_production() else "cfadagsterdev"
 
+# automatically collect Dagster definitions from the current file
 collected_defs = collect_definitions(globals())
 
-
-# Create Definitions object
+# Create Dagster definitions
 defs = dg.Definitions(
     **collected_defs,
     resources={
@@ -269,8 +257,15 @@ defs = dg.Definitions(
     executor=dynamic_executor(
         # try switching to Azure compute after pushing your image
         # default_config=default_config
-        default_config=docker_config
+        default_config=docker_config,
         # default_config=azure_caj_config
         # default_config=azure_batch_config
+        # alternate configs show you default values in the Launchpad on hover
+        alternate_configs=[
+            default_config,
+            docker_config,
+            azure_caj_config,
+            azure_batch_config,
+        ],
     ),
 )
